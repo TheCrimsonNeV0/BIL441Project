febe517cafc923d0f5fb91d5d33f769c7ca2c5ec
Nevzat Umut Demirseren <60689329+TheCrimsonNeV0@users.noreply.github.com>
2023-04-15 18:35:00 +0300

    New puzzle added

--- Puzzles.py
+++ Puzzles.py
@@ -1,3 +1,5 @@
+# Puzzles from https://lichess.org/study/WiuSw3ga/c9rkZk4L
+
 import chess
 from ChessAI import ChessAI
 
@@ -13,3 +15,11 @@ print("Puzzle 2: ", puzzle_2.calculate_best_move())  # Answer: d8h4
 puzzle_3 = ChessAI(chess.WHITE, 5)
 puzzle_3.board.set_board_fen("r2qkb1r/ppp1p1pp/2np1n2/4Nb2/3P4/1B6/PPP1PPPP/RNBQK2R")
 print("Puzzle 3: ", puzzle_3.calculate_best_move())  # Answer: b3f7
+
+puzzle_4 = ChessAI(chess.WHITE, 5)
+puzzle_4.board.set_board_fen("r2r4/ppRR3p/2p2pk1/1qB1Pp2/2b2P2/P7/1PP4P/1K3Q2")
+first = puzzle_4.calculate_best_move()
+print("Puzzle 4 step 1: ", first)  # Answer: f1g2
+puzzle_4.make_move(first)
+puzzle_4.make_move(chess.Move.from_uci("g6h6"))
+print("Puzzle 4 step 2: ", puzzle_4.calculate_best_move())  # Answer: d7h7
